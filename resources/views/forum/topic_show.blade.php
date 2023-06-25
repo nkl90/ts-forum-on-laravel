8418dcdba70714fe4a612cb6e67e368dfbc8f511
Anatoliy  Zhukov <devezor@gmail.com>
2023-06-26 00:30:24 +0600

    showTopic blade

--- resources/views/forum/topic_show.blade.php
+++ resources/views/forum/topic_show.blade.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <title>Forum</title>
-</head>
-<body>
-    <h1>Топик № {{ $uuid }} @isset ($page), страница {{ $page }} @endisset</h1>
-</body>
-</html>
+<x-layout.forum>
+    <x-slot name="title">Топик № {{ $uuid }}</x-slot>
+    <x-slot name="content">
+        <h2>Топик № {{ $uuid }}</h2>
+        <p>Страницы: @isset($page) {{ $page }} @endisset</p>
+        <div class="message_divider col-md-12"></div>
+        @each('forum.topic_message', $messages, 'message')
+        <div class="mb-3" style="display: flex; flex-direction: column; align-items: center; margin-top: 15px;">
+            <label class="form-label"><b>Оставить сообщение:</b></label>
+            <textarea class="form-control"  rows="3"></textarea>
+        </div>
+        <button type="button" class="btn btn-primary" style="display: block; margin: 0 auto;">Отправить</button> 
+    </x-slot>
+</x-layout.forum>
